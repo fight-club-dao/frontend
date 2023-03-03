77fd330994a03e5b21e177bd1bb11d312eee789c
Why Oh Why <103012174+dRadiant@users.noreply.github.com>
2023-03-03 16:18:48 -0700

    asdf

--- src/routes/Markets.tsx
+++ src/routes/Markets.tsx
@@ -1,6 +1,18 @@
 import { useState } from 'react'
 import '../App.css'  
         
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react'
+
 import { Heading } from '@chakra-ui/react'
 import { Center } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
@@ -14,16 +26,42 @@ function Markets() {
     <Box className="MainPageContainer">
       <Box className='FightClubMain'>
         <Center h="100vh">
-            <VStack spacing="8">
-                <Heading size="3xl">Fight Club</Heading>
-                <VStack spacing="3">
-                <HStack>
-                    <Button className="MainButton">Browse Markets</Button>
-                    <Button className="MainButton">Create Market</Button>
-                </HStack>
-                <Button className="MainButton">Lend</Button>
-                </VStack>
-            </VStack>
+            <TableContainer> 
+                <Table variant='simple'> 
+                    <TableCaption>Imperial to metric conversion factors</TableCaption> 
+                    <Thead> 
+                        <Tr> 
+                            <Th>To convert</Th> 
+                            <Th>into</Th> 
+                            <Th isNumeric>multiply by</Th> 
+                        </Tr> 
+                    </Thead> 
+                    <Tbody> 
+                        <Tr> 
+                            <Td>inches</Td> 
+                            <Td>millimetres (mm)</Td> 
+                            <Td isNumeric>25.4</Td> 
+                        </Tr> 
+                        <Tr> 
+                            <Td>feet</Td> 
+                            <Td>centimetres (cm)</Td> 
+                            <Td isNumeric>30.48</Td> 
+                        </Tr> 
+                        <Tr> 
+                            <Td>yards</Td> 
+                            <Td>metres (m)</Td> 
+                            <Td isNumeric>0.91444</Td> 
+                        </Tr> 
+                        </Tbody> 
+                        <Tfoot> 
+                            <Tr> 
+                                <Th>To convert</Th> 
+                                <Th>into</Th> 
+                                <Th isNumeric>multiply by</Th> 
+                            </Tr> 
+                        </Tfoot> 
+                </Table> 
+            </TableContainer>
         </Center>
       </Box>
     </Box>
